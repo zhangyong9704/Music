01ff3bcf3111bd06f0ab5110d94715eb29a7b6c1
Music <zhangyong9704@foxmail.ocm>
2020-10-26 17:27:02 +0800

    初始化环境

---
+++ src/main/java/com/cloud/music/MusicApplication.java
@@ -0,0 +1,19 @@
+package com.cloud.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>文件名称: MusicApplication.java
+ * @author ZhangYong
+ * @version v1.0
+ * @date 2020-10-26  14:27
+ */
+@SpringBootApplication
+public class MusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApplication.class, args);
+    }
+
+}
